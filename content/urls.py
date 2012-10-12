046270acf0a1c375cdef565cd76ce6bda113ed21
"guiyang" <guiyang923@gmail.com>
2012-10-12 17:25:46 +0800

    updates

--- content/urls.py
+++ content/urls.py
@@ -11,16 +11,9 @@ urlpatterns = patterns('content.views',
     url(r'content_following/', 'getFriendsProfile', {'page':'following'}),
     
     url(r'personal/', 'personal_index'),
-<<<<<<< HEAD
-    #(P<user_id>)
-    url(r'content_indxe/', 'content_index'),
-=======
     
-    url(r'content_index/', 'content_index'),
->>>>>>> branch 'master' of https://github.com/guiyang/cloudguantou.git
+    url(r'content_indxe/', 'content_index'),
     
     url(r'content_follower/', 'getFriendsProfile', {'page' :'follower'}),
-    
-    url(r'upload/', 'up_load')
     
 )
