bed3104b08190c4b9a4ddb58c841b7ddb4f2dfb7
liangchen <liangchen@192.168.2.106>
2012-10-26 11:57:11 +0800

    ccc

--- brand/models.py
+++ brand/models.py
@@ -1,9 +1,9 @@
 #coding=utf8
 from django.db import models
-from django.contrib.auth.models import User
+from authorize.models import UserProfile
 
 # Create your models here.
-class BrandProfile(models.Model):
+class Brand(models.Model):
     class Meta:
         verbose_name_plural = u'品牌信息'
         
@@ -14,15 +14,14 @@ class BrandProfile(models.Model):
     introduction = models.TextField()
     follower_count = models.IntegerField(default=0)
     favorite_count = models.IntegerField(default=0)
-    master = models.ForeignKey(User, related_name='master')
-    members = models.ManyToManyField(User, related_name='members', through='Membership')
+    master = models.ForeignKey(UserProfile, related_name='master')
     
 class Membership(models.Model):
     class Meta:
         verbose_name_plural = u'品牌成员信息'
         
-    user = models.ForeignKey(User)
-    brand = models.ForeignKey(BrandProfile)
+    user = models.ForeignKey(UserProfile)
+    brand = models.ForeignKey(Brand)
     date_joined = models.DateTimeField(auto_now=True, auto_now_add=True)
     date_dismissed = models.DateTimeField(auto_now=True, auto_now_add=True)
     available = models.BooleanField()
