0fcac9454e66f5f81953e1ca12d851dc75b8e8d7
xlk521 <1012621780@qq.com>
2012-10-29 16:41:02 +0800

    up

--- static/js/6cans.js
+++ static/js/6cans.js
@@ -982,9 +982,10 @@ function contents_getJson(imgid){//目录页---发送请求并获取数据
         success:function(msg){
             console.log("msg====>>"+msg);
             //user=msg.[];
-            //for(var i=0;i<msg.length;i++){//将新旧数据拼接到一起
-               // contents_listuser[i]=msg[i];
-            //}
+            console.log("msg.length====>>"+msg.length);
+            for(var i=0;i<msg.length;i++){//将新旧数据拼接到一起
+                contents_listuser[i]=msg[i];
+            }
             //aythor_users=msg.users;//获取数据中关于已登录作者的数据
             console.log("user_allfriends:::json====>>"+contents_listuser);
 
